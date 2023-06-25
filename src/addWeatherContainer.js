deff37377c31bea103f2a0f12d6ef0fb864b901d
Dan <dxkim201@gmail.com>
2023-06-24 22:18:17 -0400

    Worked on it a little bit today, adjusted background image and got the proper date layout in object

--- src/addWeatherContainer.js
+++ src/addWeatherContainer.js
@@ -1,3 +1,8 @@
 export default function addWeatherContainer(weatherObject){
-    
+    const container = document.createElement('div');
+
+    container.classList.add('mainWeatherContainer');
+    container.innerHTML = weatherObject.date;
+
+    return container;
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import './style.css'
 import Wallpaper from './images/wallpaperalt.jpg'
 import addWeatherContainer from './addWeatherContainer';
+
 /*
 Figure out Weather API
 
@@ -44,21 +45,44 @@ async function getWeatherData(lat,lon){
     const weather = await response.json();
 
     //Get date and format it correctly *CURRENTLY WORKING ON*
-    const date = "";
-    const dateInfo = new Date();
-    const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    const today = days[date.getDay()]
+    const date = new Date();
 
-    date.concat(today + ", ");
+    //We % 100 because I only want the last two digits of the year.
+    let year = date.getFullYear() % 100;
+    let dayDate = date.getDate();
+
+    //Gets the 'st, nd, rd, th' end for numbers
+    const day = function(d) {
+        if (d > 3 && d < 21) return 'th';
+        switch (d % 10) {
+          case 1:  return "st";
+          case 2:  return "nd";
+          case 3:  return "rd";
+          default: return "th";
+        }
+      }
+
+    //Get day and month name.
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+    let dayName = days[date.getDay()];
+    let month = months[date.getMonth()];
+
+    let currentDate = `${dayName}, ${dayDate}${day(dayDate)} ${month} '${year}`;
     
+    console.log(currentDate);
+    
+
+    //Add to weatherObject which is going to be passed around to provide weather info
     const weatherObject = {
         summary: weather.currently.summary,
         temperature: weather.currently.temperature,
         location: "New York City",
-        date: ""
+        date: currentDate,
     }
 
     //Return an object with all the data extract from API into it, because trying to pass promises through functions is no fun.
+    return weatherObject;
 }
 
 //Get current weather temperature
@@ -75,16 +99,20 @@ async function getTemperature(lat, lon){
 //Logic Application
 //-------------------------
 
-//Global Variables
+//Get weather data using function getWeatherData()
 const weatherData = getWeatherData(40.7128,-74.0060);
 
+//Body Container
 const container = document.createElement('div');
 container.id = "container";
 
-const backgroundCover = new Image();
-backgroundCover.classList.add("backgroundCover");
-backgroundCover.src = Wallpaper;
-document.body.appendChild(backgroundCover);
+//Setting background image
+
+
+document.body.style.backgroundImage = `url(${Wallpaper})`;
 
 //Create mainWeatherContainer
-addWeatherContainer(weatherData);
+container.appendChild(addWeatherContainer(weatherData));
+
+//Finally adding main container to body
+document.body.appendChild(container);
